b6ef5dd1fb56e42ed4b2ead184768ce4fd0f3576
cloudre01 <alextan01@hotmail.com>
2023-10-03 11:24:23 +0800

    update to use native relative numbers

--- lua/options.lua
+++ lua/options.lua
@@ -41,4 +41,5 @@ vim.opt.whichwrap:append "<,>,[,],h,l"          -- keys allowed to move to the p
 vim.opt.iskeyword:append "-"                    -- treats words with `-` as single words
 vim.opt.formatoptions:remove { "c", "r", "o" }  -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
-vim.opt.statuscolumn = "%s %=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum . ' ' : v:lnum) : ''}%= "
+vim.opt.relativenumber = true
+-- vim.opt.statuscolumn = "%s %=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum . ' ' : v:lnum) : ''}%= "
